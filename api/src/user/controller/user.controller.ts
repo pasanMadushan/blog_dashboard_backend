a574cbb12a50c563d6f6bc815c57930b99b36518
Pasan Madushan <pasanmadushan@gmail.com>
2022-05-29 17:06:33 +0530

    user controller and user service

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,4 +1,100 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { User } from '../models/user.interface';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService){}
+
+    @Post()
+    async create(@Body() user:User){
+        try {
+            return{
+                success:true,
+                message: "Successfully created the user",
+                data: await this.userService.create(user)
+            }
+        } catch (error) {
+            return{
+                success:false,
+                message: "Couldn't create user",
+                error:error
+            }
+        }
+    }
+
+    @Get(':id')
+    async findOne(@Param() params){
+        try {
+            return{
+                success:true,
+                message: "Successfully fetched user data",
+                data: await this.userService.findOne(params.id)
+            }
+        } catch (error) {
+            return{
+                success:false,
+                message: "Couldn't find user",
+                error:error
+            }
+        }
+    }
+
+    @Get()
+    async findAll(){
+        try {
+            return{
+                success:true,
+                message: "Successfully fetched users",
+                data: await this.userService.findAll()
+            }
+        } catch (error) {
+            return{
+                success:false,
+                message: "Couldn't fetch users",
+                error:error
+            }
+        }
+    }
+
+    @Delete(':id')
+    async deleteOne(@Param() params){
+        try {
+            return{
+                success:true,
+                message: "Successfully deleted the user",
+                data: await this.userService.deleteOne(params.id)
+            }
+        } catch (error) {
+            return{
+                success:false,
+                message: "Couldn't delete the user",
+                error:error
+            }
+        }
+    }
+
+    @Put(':id')
+    async updateOne(@Param() params, @Body() user:User){
+        try {
+            return{
+                success:true,
+                message: "Successfully updated the user",
+                data: await this.userService.updateOne(params.id, user)
+            }
+        } catch (error) {
+            return{
+                success:false,
+                message: "Couldn't update the user",
+                error:error
+            }
+        }
+    }
+
+
+
+
+
+
+}
--- api/src/user/service/user.service.ts
+++ api/src/user/service/user.service.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from } from 'node-fetch/node_modules/form-data';
+import { Observable } from 'rxjs';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../models/user.entity';
+import { User } from '../models/user.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>
+    ){}
+
+    async create(user:User): Promise<User>{
+        try{
+            return await this.userRepository.save(user);
+        }
+        catch(err){
+            throw err;
+        }   
+    }
+
+    async findOne(id:number) : Promise<User>{ 
+        try{
+            return await this.userRepository.findOne(id);
+        }
+        catch(err){
+            throw err;
+        }  
+    }
+
+    async findAll() : Promise<User[]>{ 
+        try{
+            return await this.userRepository.find();
+        }
+        catch(err){
+            throw err;
+        }  
+    }
+    
+    async deleteOne(id:number) : Promise<any>{ 
+        try{
+            return await this.userRepository.delete(id);
+        }
+        catch(err){
+            throw err;
+        }  
+    }
+
+    async updateOne(id:number, user:User) : Promise<any>{ 
+        try{
+            return await this.userRepository.update(id,user);
+        }
+        catch(err){
+            throw err;
+        }  
+    }
+}
